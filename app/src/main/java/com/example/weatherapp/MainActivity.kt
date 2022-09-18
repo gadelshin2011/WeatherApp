f19aef072309619b60e438d92ce3db4607789fad
Rustam <gadelshin2011@gmail.com>
2022-09-18 23:44:44 +0500

    Создали фрагмент MainFragment Create data class DayItem Create class MainViewModel Create news item in Manifest: connect Internet and location

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.weatherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.weatherapp.fragment.MainFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.placeHolder, MainFragment.newInstance()).commit()
+
+    }
+}
