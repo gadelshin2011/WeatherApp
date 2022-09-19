3d4145b41070682c843b956d066837f9d571bf02
Rustam <gadelshin2011@gmail.com>
2022-09-19 21:35:07 +0500

    Create new file Extension.kt - checking for permissions fix bug in Gradle create new functions in MainFragment on user request to give location permissions

--- app/src/main/java/com/example/weatherapp/fragment/MainFragment.kt
+++ app/src/main/java/com/example/weatherapp/fragment/MainFragment.kt
@@ -1,21 +1,47 @@
 package com.example.weatherapp.fragment
 
+import android.Manifest
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.weatherapp.R
 import com.example.weatherapp.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
-    lateinit var  binding: FragmentMainBinding
+    private lateinit var pLauncher: ActivityResultLauncher<String>
+    lateinit var binding: FragmentMainBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentMainBinding.inflate(inflater , container, false)
+        binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkPermission()
+    }
+
+    // показываем true or false on request location
+    private fun permissionListener() {
+        pLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            Toast.makeText(activity, "Permission is $it", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    // проверяем есть ли разрешение на местоположение, если нет выводим пользователю окно с вариантами для получения разрешения, если есть ничего не делаем
+    private fun checkPermission() {
+        if (!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            permissionListener()
+            pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
     }
 
     companion object {
