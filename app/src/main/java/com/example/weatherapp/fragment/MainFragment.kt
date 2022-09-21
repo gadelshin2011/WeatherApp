b57ecfee01d15d64d3403b03b6b904a5c184dd07
Rustam <gadelshin2011@gmail.com>
2022-09-21 22:34:28 +0500

    connect adapter to viewPager2 and TabLayout

--- app/src/main/java/com/example/weatherapp/fragment/MainFragment.kt
+++ app/src/main/java/com/example/weatherapp/fragment/MainFragment.kt
@@ -10,10 +10,23 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.FragmentActivity
 import com.example.weatherapp.R
+import com.example.weatherapp.adapters.VpAdapter
 import com.example.weatherapp.databinding.FragmentMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainFragment : Fragment() {
+    private val fList = listOf(
+        HoursFragment.newInstance(),
+        DaysFragment.newInstance()
+
+    )
+    private val tList = listOf(
+        "HOURS",
+        "DAYS"
+    )
+
     private lateinit var pLauncher: ActivityResultLauncher<String>
     lateinit var binding: FragmentMainBinding
     override fun onCreateView(
@@ -27,6 +40,15 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         checkPermission()
+        init()
+    }
+
+    private fun init() = with(binding) {
+        val adapter = VpAdapter(activity as FragmentActivity, fList)
+        vP.adapter = adapter
+        TabLayoutMediator(tabLayout, vP) {
+                tab, pos -> tab.text = tList[pos]
+        }.attach()
     }
 
     // показываем true or false on request location
