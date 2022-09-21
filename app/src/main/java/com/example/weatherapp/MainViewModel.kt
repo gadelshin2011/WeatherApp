573b1905d745f3fc4d45f5c8f19e4a0930f67a49
Rustam <gadelshin2011@gmail.com>
2022-09-21 23:13:39 +0500

    разметка страницы list_item.xml

--- app/src/main/java/com/example/weatherapp/MainViewModel.kt
+++ app/src/main/java/com/example/weatherapp/MainViewModel.kt
@@ -2,11 +2,12 @@ package com.example.weatherapp
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.weatherapp.adapters.WeatherModel
 
 class MainViewModel : ViewModel() {
 
-    val liveDataCurrent = MutableLiveData<String>()
-    val liveDataList = MutableLiveData<List<String>>()
+    val liveDataCurrent = MutableLiveData<WeatherModel>()
+    val liveDataList = MutableLiveData<List<WeatherModel>>()
 
 
 }
